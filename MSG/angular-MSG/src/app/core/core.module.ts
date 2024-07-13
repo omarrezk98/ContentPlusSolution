bf4639bb7a0a4c807ffd4ac9db11f0b3ac05a007
eem2001 <159649763+eem2001@users.noreply.github.com>
2024-07-13 19:42:34 +0300

    work in MSG

--- MSG/angular-MSG/src/app/core/core.module.ts
+++ MSG/angular-MSG/src/app/core/core.module.ts
@@ -4,9 +4,10 @@ import { HeaderComponent } from './theme/header/header.component';
 import { FooterComponent } from './theme/footer/footer.component';
 import { LayoutComponent } from './theme/layout/layout.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { SliderComponent } from './theme/slider/slider.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, LayoutComponent],
+  declarations: [HeaderComponent, FooterComponent, LayoutComponent, SliderComponent],
   imports: [CommonModule, TranslateModule],
   exports: [HeaderComponent, FooterComponent, LayoutComponent, TranslateModule],
 })
